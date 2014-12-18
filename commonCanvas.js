f1f16133f08c3f7ec5603ae112fb01d47778e0d6
stuzeakd <stuzeakd@nhnnext.org>
2014-12-18 16:34:33 +0900

    add methods Canvas, setPixel, and arrange rgb2Hex.

--- commonCanvas.js
+++ commonCanvas.js
@@ -19,13 +19,32 @@ if(isNodeModule){
 }
 
 var commonCanvas = {
-    createCanvasByImage : function(img, saturate){
+    Canvas : Canvas,
+    
+    setPixel : function(target, x, y, r, g, b, a){
+        color = typeof r === 'object' ? r : {r: r, g: g, b: b, a: a};
+        if(target.constructor.name === "CanvasRenderingContext2D"){
+            target.fillStyle = 'rgba('+color.r+','+color.g+','+color.b+','+rolor.a+')';
+            target.fillRect(x,y,1,1);
+        } else if(target.constructor.name === "ImageData"){
+            if(a < 1) parseInt( a * 255 );
+            var index = (y * target.width + x) * 4;
+            target.data[index + 0] = color.r;
+            target.data[index + 1] = color.g;
+            target.data[index + 2] = color.b;
+            target.data[index + 3] = color.a;
+        } else {
+            throw target;
+        }
+    },
+    
+    createCanvasByImage : function(img, pixelSaturate){
     //    console.log(__basename + " - function() createCanvasByImage start ...");
     //    console.log(img);
 
         var pixelNum = img.width * img.height;
-        saturate = typeof saturate !== "undefined" ? saturate : pixelNum;    
-        var pixelNumRate = pixelNum / saturate;
+        pixelSaturate = typeof pixelSaturate !== "undefined" ? pixelSaturate : pixelNum;    
+        var pixelNumRate = pixelNum / pixelSaturate;
 
         var canvasWidth = img.width;
         var canvasHeight = img.height;
@@ -43,7 +62,25 @@ var commonCanvas = {
         rCanvasCtx.drawImage(img, 0,0, img.width, img.height, 0,0, canvasWidth, canvasHeight);
     //    console.log(__basename + " - function() createCanvasByImage end");
         return rCanvas;
-    } 
+    },
+    hex2Rgb : function(hex){    //#ff1234
+        var r = parseInt(hex.slice(1,3),16);
+        var g = parseInt(hex.slice(3,5),16);
+        var b = parseInt(hex.slice(5,7),16);
+        return {r: r, g: g, b: b, a: 255};
+    },
+    rgb2Hex : function(r,g,b){
+        if(typeof r === 'object'){
+            g = r.g;
+            b = r.b;
+            r = r.r;
+        }
+        function dec2HexStr(d){   
+            var hStr = d.toString(16);
+            return hStr.length === 1? "0" + hStr : hStr;
+        }
+        return "#"+dec2HexStr(r)+dec2HexStr(g)+dec2HexStr(b);
+    }
 };
     
     
